63016de940c9a72fbcbf43e7ff533300e31db1c8
Khimiy Maxim <khimiy.maxym@gmail.com>
2015-04-20 15:09:19 +0300

    whole project

---
+++ src/BankingSite.UITests/LogicalFunctionalModelStyle/CreditCardApplicationTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace BankingSite.UITests.LogicalFunctionalModelStyle
+{
+    [TestFixture]    
+    public class CreditCardApplicationTests
+    {
+        [Test]
+        [STAThread]
+        [Category("smoke")]
+        public void ShouldShowCorrectApplicantDetailsOnSuccessPage()
+        {
+              using (var browser =
+                new IE(UiAutomationSettings.ApplyPageUrl))
+            {
+                var applyPage = browser.Page<ApplyForCreditCardPage>();
+
+                applyPage.ApplyForCreditCard(name: "Jason",
+                                age: "30",
+                                airlineNumber: "A1234567");
+
+                Assert.That(browser.Url.Contains("ApplicationAccepted.aspx"));
+
+                var acceptedPage = browser.Page<AcceptedPage>();
+
+                Assert.That(acceptedPage.Document.Url.Contains("ApplicationAccepted.aspx"));
+
+                Assert.That(acceptedPage.Name, Is.EqualTo("Jason"));
+            }
+        }
+
+
+        [Test]
+        [STAThread]
+        public void ShouldShowValidationErrors()
+        {
+            using (var browser =
+                new IE(UiAutomationSettings.ApplyPageUrl))
+            {
+                var applyPage = browser.Page<ApplyForCreditCardPage>();
+
+                applyPage.ApplyForCreditCard(name: "Jason",
+                                             age: "30",
+                                             airlineNumber: "BadNumber");
+                
+                Assert.That(browser.Text.Contains("Airline membership number is invalid"));
+            }
+        }
+
+        [Test]
+        [STAThread]
+        public void DummyTest1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        [STAThread]
+        public void DummyTest2()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        [STAThread]
+        public void DummyTest3()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        [STAThread]
+        public void DummyTest4()
+        {
+            Assert.Pass();
+        }
+    }
+}
